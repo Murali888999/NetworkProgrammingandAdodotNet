f7f7b0cd17c6f9458065204cb4628eb23be9bd59
Murali888999 <mohanggk24@gmail.com>
2018-03-28 19:49:11 +0530

    NetworkProgrammingandAdodotNet

---
+++ Server3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Server3
+    {
+        static void Main(string[] args)
+        {
+            Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sck.Bind(new IPEndPoint(IPAddress.Any, 101));
+            sck.Listen(1);
+            Console.WriteLine("socket listing........");
+
+            Socket Client = default(Socket);
+            int count = 0;
+            Server3 s = new Server3();
+            while (true)
+            {
+                count++;
+                Client = sck.Accept();
+                Console.WriteLine(count + "connected........");
+                Thread userthread1 = new Thread(new ThreadStart(() => s.user(Client)));
+                userthread1.Start();
+            }
+        }
+        private void user(Socket client)
+        {
+            while (true)
+            {
+                byte[] readb = new byte[1000];
+                int data = client.Receive(readb);
+                String s1 = Encoding.ASCII.GetString(readb, 0, data);
+                Console.WriteLine(s1);
+                client.Send(readb, 0, data, SocketFlags.None);
+            }
+        }
+    }
+}
---
+++ client3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client1
+{
+
+    class client3
+    {
+        static void Main(string[] args)
+        {
+            Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 101);
+            sck.Connect(endPoint);
+            Console.WriteLine("Connected.");
+            while (true)
+            {
+                String s = null;
+                Console.WriteLine("Enter Message");
+                s = Console.ReadLine();
+                sck.Send(Encoding.ASCII.GetBytes(s), 0, s.Length, SocketFlags.None);
+
+                byte[] msgfromServer = new byte[1000];
+                int size = sck.Receive(msgfromServer);
+                Console.WriteLine("from server " + Encoding.ASCII.GetString(msgfromServer, 0, size));
+            }
+        }
+    }
+}
